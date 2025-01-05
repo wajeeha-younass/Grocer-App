5bee332b94625ff5336a298ef15997e38a869720
wajeeha-younass <wajeehayounas775@gmail.com>
2025-01-05 23:52:51 +0500

    Fouth Deliverable

--- app/src/main/java/com/example/app/Category.kt
+++
@@ -1,7 +0,0 @@
-package com.example.app
-
-data class Category(
-    val imageResId: Int,
-    val title: String,
-    val description: String
-)
--- app/src/main/java/com/example/app/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.app
-
-data class Product(
-   val name: String,
-   val quantity: String,
-   val originalPrice: Int,
-   val discountedPrice: Int,
-   val discount: String
-)
